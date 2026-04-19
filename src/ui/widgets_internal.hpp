f6c4e03657e4bbd2c22ab5618841e104e7d53d36
ErikMN <yubnut@gmail.com>
2026-04-19 16:10:57 +0200

    widgets_internal.hpp: Remove uneeded comment

--- src/ui/widgets_internal.hpp
+++ src/ui/widgets_internal.hpp
@@ -11,7 +11,6 @@ struct SearchWidgets;
 GCancellable *widgets_make_task_cancellable_for(GtkWidget *w);
 void widgets_spinner_acquire(GtkSpinner *spinner);
 void widgets_spinner_release(GtkSpinner *spinner);
-// Run a non-blocking Base rebuild and its GTK completion handler.
 void widgets_on_rebuild_task(GTask *task, gpointer, gpointer, GCancellable *);
 void widgets_on_rebuild_task_finished(GObject *, GAsyncResult *res, gpointer user_data);
 
